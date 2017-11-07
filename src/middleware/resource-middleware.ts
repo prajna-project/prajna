545537b0633892928ebbd27b402f6a9c1aefc34b
YoungLee <youngleemails@gmail.com>
2017-11-07 20:45:47 +0800

    resource error

--- src/middleware/resource-middleware.ts
+++ src/middleware/resource-middleware.ts
@@ -3,11 +3,48 @@ import GLOBAL from '../util/global';
 import Message from '../core/types/message.type';
 import Resource from '../core/types/resource.type';
 import getEntries, { PerformanceObserver } from '../util/getEntries';
+import {
+    LogLevel
+} from '../core/types/log.type';
 
 let FORMER_RESOURCE_FLAG: boolean = true;
 let ENTRY_SIZE = 0;
 
+function _sendErrors(ctx: any) {
+    let cache: any = ls.get('prajna_cache') || {};
+    let cachedRes: Resource[] = cache.resource || [];
+    let mergedData: Message[] = [];
+    if (cachedRes.length) {
+        cachedRes.forEach(function (e: Resource, i: number) {
+            let raw = ctx.inspect();
+            e.pageId = ctx.core.pageId;
+            e.pageUrl = GLOBAL.location.href;
+            raw.resource = e;
+            mergedData.push(raw);
+        });
+        ctx.core.emit(LogLevel.ERROR);
+        let _xhr: XMLHttpRequest = new XMLHttpRequest();
+        _xhr.open('POST', ctx.core.url + '/api/prajna', true);
+        _xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        _xhr.onreadystatechange = function (e) {
+            if (_xhr.readyState == 4) {
+                if (_xhr.status == 200) {
+                    cache.resource = [];
+                    ls.set('prajna_cache', cache);
+                } else {
+                    // TODO:
+                }
+            } else {
+                // TODO:
+            }
+        };
+        _xhr.onerror = function (e) { console.log(e); };
+        _xhr.send('data=' + encodeURIComponent(JSON.stringify(mergedData)));
+    }
+}
+
 function _sendEntries(ctx: any, ent: PerformanceEntry[]): void {
+    _sendErrors(ctx);
     let match = function (name: string, whitelist: RegExp[]) {
         for (let i: number = 0; i < whitelist.length; i++) {
             if (whitelist[i].test(name) === true) {
@@ -23,14 +60,17 @@ function _sendEntries(ctx: any, ent: PerformanceEntry[]): void {
                 match(e.name, ctx.core.whitelist) === false &&
                 e.initiatorType !== "xmlhttprequest" &&
                 e.initiatorType !== "beacon") {
-                e.resourceUrl = e.name;
-                e.unix = +new Date();
-                e.pageId = ctx.core.pageId;
-                e.pageUrl = GLOBAL.location.href;
-                e.status = 200;
-                e.responsetime = e.duration;
-                delete e.name;
-                res.push(e);
+                let strE = JSON.stringify(e);
+                let _e = JSON.parse(strE); // copy
+                _e.resourceUrl = _e.name;
+                _e.unix = +new Date();
+                _e.pageId = ctx.core.pageId;
+                _e.pageUrl = GLOBAL.location.href;
+                _e.status = 200;
+                _e.responsetime = _e.duration;
+                delete _e.name;
+                delete _e.duration;
+                res.push(_e);
             }
         });
         return res;
@@ -55,22 +95,30 @@ function _sendEntries(ctx: any, ent: PerformanceEntry[]): void {
         _xhr.onreadystatechange = function (e) {
             if (_xhr.readyState == 4) {
                 if (_xhr.status == 200) {
-                    // cache.log = [];
-                    // ls.set('prajna_cache', cache);
+                    // TODO:
                 } else {
-                    // cache.log = cachedLog;
-                    // ls.set('prajna_cache', cache);
+                    let cache: any = ls.get('prajna_cache') || {};
+                    let cachedRes: any[] = cache.resource || [];
+                    for (let i: number = 0; i < resources.length; i++) {
+                        cachedRes.push(resources[i]);
+                    }
+                    cache.resource = cachedRes;
+                    ls.set('prajna_cache', cache);
                 }
             } else {
-                // cache.log = cachedLog;
-                // ls.set('prajna_cache', cache);
+                let cache: any = ls.get('prajna_cache') || {};
+                let cachedRes: any[] = cache.resource || [];
+                for (let i: number = 0; i < resources.length; i++) {
+                    cachedRes.push(resources[i]);
+                }
+                cache.resource = cachedRes;
+                ls.set('prajna_cache', cache);
             }
         };
         _xhr.onerror = function (e) { console.log(e); };
         _xhr.send('data=' + encodeURIComponent(JSON.stringify(mergedData)));
     }
 }
-
 
 function _resourceRuntime(ctx: any): void {
     GLOBAL.addEventListener("load", () => {
@@ -85,11 +133,16 @@ function resourceMiddleware(ctx: any, next: any): any {
     if (FORMER_RESOURCE_FLAG) {
         FORMER_RESOURCE_FLAG = false;
         _resourceRuntime(ctx);
+        // if (PerformanceObserver) {
+        // 	// TODO:
+        // } else {
+        //     // TODO:
+        // }
         setInterval(() => {
             if (getEntries) {
                 _sendEntries(ctx, GLOBAL.performance.getEntries());
             }
-        }, 2000);
+        }, 1000);
     }
     next();
 }
