f78d9a3098de8af0aacd7a4dc1e9205a1cb1bb6e
jjvein <jjvein@gmail.com>
2017-11-22 14:18:56 +0800

    修改 registry

--- init.sh
+++ init.sh
@@ -5,13 +5,13 @@ git fetch origin
 git merge origin/master
 
 npm install --registry=https://registry.npm.taobao.org -g webpack
-npm install --registry=https://registry.npm.taobao.org -g karma
-npm install --registry=https://registry.npm.taobao.org
+npm install -g karma
+npm install
 npm run build
 
 cd test/browser-test
 
-npm install --registry=https://registry.npm.taobao.org
+npm install
 
 npm run build-test-context
 
